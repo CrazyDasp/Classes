e25b27968d1fad6690ff84932858b753808ae2a7
Roman <kameta-r3@yandex.ru>
2024-10-29 12:09:39 +0300

    Init

---
+++ src/js/__tests__/characters.test.js
@@ -0,0 +1,121 @@
+import * as CreateChar from "../characters";
+
+test("Testing create bowerman", () => {
+    let bowemanChar = new CreateChar.Bowerman("Legolas", "Bowerman")
+
+    expect(bowemanChar).toEqual({
+        "name": "Legolas",
+        "type": "Bowerman",
+        "health": 100,
+        "level": 1,
+        "attack": 25,
+        "defence": 25
+    })
+})
+
+test("Testing create swordsman", () => {
+    let bowemanChar = new CreateChar.Swordsman("Varian", "Swordsman")
+
+    expect(bowemanChar).toEqual({
+        "name": "Varian",
+        "type": "Swordsman",
+        "health": 100,
+        "level": 1,
+        "attack": 40,
+        "defence": 10
+    })
+})
+
+test("Testing create magician", () => {
+    let bowemanChar = new CreateChar.Magician("Khadgar", "Magician")
+
+    expect(bowemanChar).toEqual({
+        "name": "Khadgar",
+        "type": "Magician",
+        "health": 100,
+        "level": 1,
+        "attack": 10,
+        "defence": 40
+    })
+})
+
+test("Testing create undead", () => {
+    let bowemanChar = new CreateChar.Undead("Sylvanas", "Undead")
+
+    expect(bowemanChar).toEqual({
+        "name": "Sylvanas",
+        "type": "Undead",
+        "health": 100,
+        "level": 1,
+        "attack": 25,
+        "defence": 25
+    })
+})
+
+test("Testing create zombie", () => {
+    let bowemanChar = new CreateChar.Zombie("Arthas", "Zombie")
+
+    expect(bowemanChar).toEqual({
+        "name": "Arthas",
+        "type": "Zombie",
+        "health": 100,
+        "level": 1,
+        "attack": 40,
+        "defence": 10
+    })
+})
+
+test("Testing create daemon", () => {
+    let bowemanChar = new CreateChar.Daemon("Sargeras", "Daemon")
+
+    expect(bowemanChar).toEqual({
+        "name": "Sargeras",
+        "type": "Daemon",
+        "health": 100,
+        "level": 1,
+        "attack": 10,
+        "defence": 40
+    })
+})
+
+test("Testing wrong character with name more than 10 letters", () => {
+    expect(() => {new CreateChar.Bowerman("SuperMegaShooter", "Bowerman")}).toThrowError()
+})
+
+test("Testing wrong character with wrong type", () => {
+    expect(() => {new CreateChar.Bowerman("Legolas", "Hunter")}).toThrowError()
+})
+
+test("Testing level up character", () => {
+    let bowemanChar = new CreateChar.Bowerman("Legolas", "Bowerman")
+    bowemanChar.levelUp()
+
+    expect(bowemanChar).toEqual({
+        "name": "Legolas",
+        "type": "Bowerman",
+        "health": 100,
+        "level": 2,
+        "attack": 31.25,
+        "defence": 31.25
+    })
+})
+
+test("Testing level up dead character", () => {
+    let bowemanChar = new CreateChar.Bowerman("Legolas", "Bowerman")
+    bowemanChar.damage(500)
+
+    expect(() => {bowemanChar.levelUp()}).toThrowError()
+})
+
+test("Testing damage dead character", () => {
+    let bowemanChar = new CreateChar.Bowerman("Legolas", "Bowerman")
+    bowemanChar.damage(500)
+
+    expect(() => {bowemanChar.damage(50)}).toThrowError()
+})
+
+test("Testing kill character", () => {
+    let bowemanChar = new CreateChar.Bowerman("Legolas", "Bowerman")
+
+    expect(bowemanChar.damage(500)).toEqual(console.log("Wasted"))
+})
---
+++ src/js/characters.js
@@ -0,0 +1,104 @@
+export class Character{
+    constructor(name, type, health, level, attack, defence) {
+        if (name.length <= 10 && name.length >= 2) {
+            this.name = name
+        } else {
+            throw new Error("Error: name must be contains from 2 to 10 letters")
+        };
+
+        if (["Bowerman", "Swordsman", "Magician", "Undead", "Daemon", "Zombie"].includes(type)) {
+            this.type = type
+        } else {
+            throw new Error("Error: unsupported type of character")
+        }
+
+        this.health = health;
+        this.level = level;
+        this.attack = attack;
+        this.defence = defence;
+      
+    }
+
+    levelUp() {
+      if (this.health > 0) {
+        this.health = 100;
+        this.level += 1;
+        this.attack *= 1.25;
+        this.defence *= 1.25;
+      } else {
+        throw new Error("Error: can't level up dead character")
+      }
+    }
+  
+    damage(points) {
+        if (this.health > 0) {
+            this.health -= points * (1 - this.defence / 100)
+        } else {
+            throw new Error("Error: character already dead")
+        };
+      
+        if (this.health <= 0) {
+            console.log("Wasted")
+        }
+    };
+}
+
+export class Bowerman extends Character {
+    constructor(name, type) {
+        super(name, type);
+        this.health = 100;
+        this.level = 1;
+        this.attack = 25;
+        this.defence = 25;
+    }
+}
+
+export class Swordsman extends Character {
+    constructor(name, type) {
+        super(name, type);
+         this.health = 100;
+         this.level = 1;
+          this.attack = 40;
+         this.defence = 10;
+    }
+}
+
+export class Magician extends Character {
+    constructor(name, type) {
+         super(name, type);
+         this.health = 100;
+         this.level = 1;
+         this.attack = 10;
+        this.defence = 40;
+    }
+}
+
+export class Undead extends Character {
+    constructor(name, type) {
+         super(name, type);
+        this.health = 100;
+         this.level = 1;
+        this.attack = 25;
+        this.defence = 25;
+    }
+}
+
+export class Zombie extends Character {
+    constructor(name, type) {
+        super(name, type);
+         this.health = 100;
+         this.level = 1;
+         this.attack = 40;
+         this.defence = 10;
+    }
+}
+
+export class Daemon extends Character {
+    constructor(name, type) {
+        super(name, type);
+         this.health = 100;
+         this.level = 1;
+         this.attack = 10;
+         this.defence = 40;
+    }
+}
